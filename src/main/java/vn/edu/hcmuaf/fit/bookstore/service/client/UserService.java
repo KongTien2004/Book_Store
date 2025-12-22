c9ea407f260aa7e2d53eca0a80a15ff4e71bf836
KongTien2004 <22130131@st.hcmauf.edu.vn>
2025-12-22 11:20:35 +0700

    feat (service): Tạo lớp UserService lưu trữ cách xử lý thông tin tài khoản khách hàng

--- src/main/java/vn/edu/hcmuaf/fit/bookstore/service/client/UserService.java
+++ src/main/java/vn/edu/hcmuaf/fit/bookstore/service/client/UserService.java
@@ -1,5 +1,24 @@
 package vn.edu.hcmuaf.fit.bookstore.service.client;
 
-public class UserService {
+import vn.edu.hcmuaf.fit.bookstore.DAO.client.UserDAO;
+import vn.edu.hcmuaf.fit.bookstore.model.Users;
 
+public class UserService {
+    static UserDAO userDAO = new UserDAO();
+
+    public Users checkUsername(String username) {
+        return userDAO.checkUsername(username);
+    }
+
+    public Users checkLogin(String username, String password) {
+        return userDAO.checkLogin(username, password);
+    }
+
+    public boolean registerUser(Users user) {
+        return userDAO.registerUser(user);
+    }
+
+    public boolean updatePassword(int userId, String newPassword) {
+        return userDAO.updatePassword(userId, newPassword);
+    }
 }
