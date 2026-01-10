c27ace4c92f51272088b1d946297eac5407e7571
LePhiVu20130468 <20130468@st.hcmuaf.edu.vn>
2026-01-10 23:22:52 +0700

    update database name in interface ConstantUtils

--- src/main/java/vn/edu/hcmuaf/fit/bookstore/utils/ConstantUtils.java
+++ src/main/java/vn/edu/hcmuaf/fit/bookstore/utils/ConstantUtils.java
@@ -3,7 +3,7 @@ package vn.edu.hcmuaf.fit.bookstore.utils;
 public interface ConstantUtils {
     int DB_PORT = 3306;
     String SERVER_NAME = "localhost";
-    String DB_NAME = "bookshopdb";
+    String DB_NAME = "book_store";
     String DB_USERNAME = "root";
     String DB_PASSWORD = "";
     String IMAGE_PATH = "C:\\var\\webapp\\images"; // Dùng "C:\\var\\webapp\\images" trên Windows
